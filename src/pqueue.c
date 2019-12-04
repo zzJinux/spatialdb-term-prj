6a727a839ead7459ea9dc4c5d2b26aeed56e3dcf
Jinwook Jeong <vustthat@gmail.com>
2019-12-05 00:04:22 +0900

    feat(pqueue): comparator is context-aware

--- src/pqueue.c
+++ src/pqueue.c
@@ -1,10 +1,11 @@
 #include "pqueue.h"
 #include <string.h>
 
-struct pqueue create_pq(size_t elem_sz, int (*cmp)(void const *, void const *)) {
+struct pqueue create_pq(size_t elem_sz, int (*cmp)(void *, void const *, void const *), void *aux) {
     struct pqueue pq;
     pq._arr = create_array(1, elem_sz);
     pq._cmp = cmp;
+    pq._aux = aux;
     return pq;
 }
 
@@ -20,7 +21,7 @@ void pq_push(struct pqueue *pq, void *elem) {
     resize_array(&pq->_arr, pq->_arr.len + 1);
     size_t elem_size = pq->_arr.elem_size;
     int i = pq->_arr.len - 1;
-    while(i > 1 && pq->_cmp(elem, GET(pq->_arr, i/2)) > 0) {
+    while(i > 1 && pq->_cmp(pq->_aux, elem, GET(pq->_arr, i/2)) > 0) {
         memcpy(GET(pq->_arr, i), GET(pq->_arr, i/2), elem_size);
         i/=2;
     }
@@ -35,11 +36,11 @@ void pq_pop(struct pqueue *pq) {
 
     int i = 2;
     while(i < post_arr_sz) {
-        if(i+1 < post_arr_sz && pq->_cmp(GET(pq->_arr, i+1), GET(pq->_arr, i)) > 0) {
+        if(i+1 < post_arr_sz && pq->_cmp(pq->_aux, GET(pq->_arr, i+1), GET(pq->_arr, i)) > 0) {
             ++i;
         }
 
-        if(pq->_cmp(p, GET(pq->_arr, i)) >= 0) break;
+        if(pq->_cmp(pq->_aux, p, GET(pq->_arr, i)) >= 0) break;
 
         memcpy(GET(pq->_arr, i/2), GET(pq->_arr, i), elem_size);
         i *= 2;
--- src/pqueue.h
+++ src/pqueue.h
@@ -5,10 +5,11 @@
 
 struct pqueue {
     struct array _arr;
-    int (*_cmp)(void const *, void const *);
+    int (*_cmp)(void *, void const *, void const *);
+    void *_aux;
 };
 
-struct pqueue create_pq(size_t, int (*)(void const *, void const *));
+struct pqueue create_pq(size_t, int (*)(void *, void const *, void const *), void *);
 void destroy_pq(struct pqueue *pq);
 size_t pq_size(struct pqueue *pq);
 void pq_push(struct pqueue *pq, void *elem);
